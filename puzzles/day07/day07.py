81daf4c3c2eb8f8ec4dc0fd01c2485eba4e22e5e
Steve Wilson <steve.wilson256@gmail.com>
2023-12-09 22:26:05 +1100

    Make it faster

--- puzzles/day07/day07.py
+++ puzzles/day07/day07.py
@@ -1,6 +1,6 @@
-import argparse
+import sys
 from collections import Counter
-from dataclasses import dataclass
+# Not using type hints or argparse because they add ~10ms to startup time
 
 # Converts 2-9,T,J,Q,K,A to 2-9,a,b,c,d,e so that strings
 # can be sorted according to value. i.e. A < K < Q < J < T etc.
@@ -10,6 +10,10 @@ SORTABLE_CARD_TRANSLATION = {
     ord('Q'): ord('c'),
     ord('K'): ord('d'),
     ord('A'): ord('e'),
+}
+# For part 2 the joker is converted to the lowest value
+PART2_JOKER_TRANSLATION = {
+    ord('b'): ord('0'),
 }
 
 
@@ -22,7 +26,7 @@ ONE_PAIR = '2'
 HIGH_CARD = '1'
 
 
-def hand_type(card_counter: Counter) -> str:
+def hand_type(card_counter):
     most_common = card_counter.most_common()
     unique_cards = len(most_common)
     if unique_cards == 5:
@@ -31,65 +35,52 @@ def hand_type(card_counter: Counter) -> str:
         return ONE_PAIR
     elif unique_cards == 3:
         # Either two pair, or three of a kind
-        _, cards_in_best_group = most_common[0]
+        cards_in_best_group = most_common[0][1]
         if cards_in_best_group == 2:
             return TWO_PAIR
-        assert cards_in_best_group == 3
         return THREE_OF_A_KIND
     elif unique_cards == 2:
         # Either a full house, or four of a kind
-        _, cards_in_best_group = most_common[0]
+        cards_in_best_group = most_common[0][1]
         if cards_in_best_group == 3:
             return FULL_HOUSE
 
-        assert cards_in_best_group == 4
         return FOUR_OF_A_KIND
 
     # Five of a kind
-    assert unique_cards == 1
     return FIVE_OF_A_KIND
 
 
-@dataclass
 class Hand:
-    cards: str
-    sortable_cards_part1: str
-    sortable_cards_part2: str
-    bid: int
-
-    @staticmethod
-    def from_line(line: str) -> "Hand":
+    def __init__(self, line):
         # Example line: '32T3K 765'
-        cards, bid = line.strip().split()
-        card_counter = Counter(cards)
+        cards, bid = line.split()
+        self.bid = int(bid)
         translated_cards = cards.translate(SORTABLE_CARD_TRANSLATION)
-        # Part 1, prefix hand with the type of hand it is to make it sortable
-        hand_type_part1 = hand_type(card_counter)
-        sortable_cards_part1 = hand_type_part1 + translated_cards
 
-        # Part 2, count the jokers and add them to the most common other card
+        # Part 1, prefix hand type to translated cards to make a value which can be used to sort
+        card_counter = Counter(cards)
+        hand_type_part1 = hand_type(card_counter)
+        self.sortable_cards_part1 = hand_type_part1 + translated_cards
+
+        # Part 2, add the joker count to the most common other card, then do the same thing as part 1
         joker_count = card_counter["J"]
         if joker_count == 0:
-            hand_type_part2 = hand_type_part1
+            # No changes, re-use result
+            self.sortable_cards_part2 = self.sortable_cards_part1
         elif joker_count == 5:
-            hand_type_part2 = FIVE_OF_A_KIND
+            # All jokers, 5 of a kind
+            self.sortable_cards_part2 = FIVE_OF_A_KIND + '00000'
         else:
             del card_counter["J"]
             card_counter[card_counter.most_common()[0][0]] += joker_count
             hand_type_part2 = hand_type(card_counter)
-        sortable_cards_part2 = hand_type_part2 + translated_cards.replace("b", "0")  # Jokers are now worth the least
-
-        return Hand(
-            cards=cards,
-            sortable_cards_part1=sortable_cards_part1,
-            sortable_cards_part2=sortable_cards_part2,
-            bid=int(bid),
-        )
+            self.sortable_cards_part2 = hand_type_part2 + translated_cards.translate(PART2_JOKER_TRANSLATION)
 
 
 def puzzle(filename):
     with open(filename, "r") as f:
-        hands = [Hand.from_line(line) for line in f.readlines()]
+        hands = [Hand(line) for line in f.readlines()]
 
     part1_sorted = sorted(hands, key=lambda h: h.sortable_cards_part1)
     part1 = sum((
@@ -107,7 +98,4 @@ def puzzle(filename):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    args = parser.parse_args()
-    print(puzzle(args.filename))
+    print(puzzle(sys.argv[1]))
