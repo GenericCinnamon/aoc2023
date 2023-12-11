cde5bae2dfca0497b3a2461839e02ecc62c0c52c
Steve Wilson <steve.wilson256@gmail.com>
2023-12-11 22:41:50 +1100

    Part 2

--- puzzles/day05/day05.py
+++ puzzles/day05/day05.py
@@ -1,29 +1,53 @@
 import argparse
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Tuple
+
+
+@dataclass
+class Range:
+    lower: int  # inclusive
+    upper: int  # not-inclusive
+    length: int = field(init=False)
+
+    def __post_init__(self):
+        self.length = max(0, self.upper - self.lower)
 
 
 @dataclass
 class Mapping:
-    destination_range_start: int
-    source_range_start: int
-    range_length: int
+    destination_range: Range
+    source_range: Range
 
     @staticmethod
     def from_line(line: str) -> "Mapping":
-        dest, src, length = line.strip().split()
+        dest, src, range_length = line.strip().split()
+        length = int(range_length)
+        dest_int = int(dest)
+        src_int = int(src)
         return Mapping(
-            destination_range_start=int(dest),
-            source_range_start=int(src),
-            range_length=int(length),
+            destination_range=Range(dest_int, dest_int + length),
+            source_range=Range(src_int, src_int + length),
         )
 
-    def __contains__(self, value: int):
-        return self.source_range_start <= value < self.source_range_start + self.range_length
+    def convert_src_to_dst(self, src: Range) -> Tuple[Range, Range, Range]:
+        """
+        src :            x----------------x
+        source_range:        o-------o
+        Divides into at most three new ranges.
+        This function always returns three ranges,
+        however some may have zero or negative length and should thus be ignored
+        """
+        lower_unmapped = Range(src.lower, min(src.upper, self.source_range.lower))
+        upper_unmapped = Range(max(src.lower, self.source_range.upper), src.upper)
+        to_map = Range(max(src.lower, self.source_range.lower), min(src.upper, self.source_range.upper))
+        mapped = Range(
+            self.destination_range.lower + (to_map.lower - self.source_range.lower),
+            self.destination_range.lower + (to_map.upper - self.source_range.lower),
+        )
 
-    def convert_src_to_dst(self, src: int) -> int:
-        assert src in self
-        return self.destination_range_start + (src - self.source_range_start)
+        assert src.length == lower_unmapped.length + mapped.length + upper_unmapped.length
+
+        return lower_unmapped, mapped, upper_unmapped
 
 
 @dataclass
@@ -36,39 +60,72 @@ class Map:
         name, *lines = block.split("\n")
         return Map(name.split()[0], list(map(Mapping.from_line, lines)))
 
-    def convert_src_to_dst(self, src: int):
+    def convert_src_to_dst(self, src: Range) -> List[Range]:
+        done: List[Range] = []
+        todo = [src]
         for mapping in self.mappings:
-            if src in mapping:
-                return mapping.convert_src_to_dst(src)
-        return src
+            next_todo = []
+            for range in todo:
+                lower_unmapped, mapped, upper_unmapped = mapping.convert_src_to_dst(range)
+                if mapped.length > 0:
+                    done.append(mapped)
+                if lower_unmapped.length > 0:
+                    next_todo.append(lower_unmapped)
+                if upper_unmapped.length > 0:
+                    next_todo.append(upper_unmapped)
+                assert lower_unmapped.length or mapped.length or upper_unmapped.length
+            todo = next_todo
+        done.extend(todo)
+
+        lengths = sum((r.length for r in done))
+        assert lengths == src.length, f"{lengths=} != {src.length=}"
+        return done
 
 
 @dataclass
 class Almanac:
-    seeds: List[int]
+    part1_seeds: List[Range]
+    part2_seeds: List[Range]
     maps: List[Map]
 
     @staticmethod
     def from_content(content: str) -> "Almanac":
         seeds_section, *map_sections = content.split("\n\n")
+        seed_ints = list(map(int, seeds_section.split()[1:]))
+        part1_seeds = [Range(s, s+1) for s in seed_ints]
+        part2_seeds = [Range(seed_ints[i], seed_ints[i] + seed_ints[i+1]) for i in range(0, len(seed_ints), 2)]
         return Almanac(
-            list(map(int, seeds_section.split()[1:])),
+            part1_seeds,
+            part2_seeds,
             list(map(Map.from_block, map_sections)),
         )
 
-    def convert(self, src: int):
+    def convert(self, seed: Range) -> List[Range]:
+        seeds = [seed]
         for mapp in self.maps:
-            src = mapp.convert_src_to_dst(src)
-        return src
+            next_seeds = []
+            for s in seeds:
+                next_seeds.extend(mapp.convert_src_to_dst(s))
+            seeds = next_seeds
+        return seeds
 
 
 def puzzle(filename):
     with open(filename, "r") as f:
         almanac = Almanac.from_content(f.read())
 
-    part1 = min((almanac.convert(s) for s in almanac.seeds))
+    part1 = min((
+        result.lower
+        for s in almanac.part1_seeds
+        for result in almanac.convert(s)
+    ))
+    part2 = min((
+        result.lower
+        for s in almanac.part2_seeds
+        for result in almanac.convert(s)
+    ))
 
-    return (part1, 2)
+    return (part1, part2)
 
 
 if __name__ == "__main__":
--- tests/test_day05.py
+++ tests/test_day05.py
@@ -1,6 +1,8 @@
+from unittest.mock import ANY
+
 import pytest
 
-from puzzles.day05.day05 import Almanac, Map, Mapping
+from puzzles.day05.day05 import Almanac, Map, Mapping, Range
 
 
 @pytest.fixture
@@ -14,34 +16,43 @@ def test_almanac_parsing(almanac: Almanac):
     assert almanac.maps[1].name == "soil-to-fertilizer"
 
     mappings = almanac.maps[0].mappings
-    assert mappings[0].destination_range_start == 50
-    assert mappings[0].source_range_start == 98
-    assert mappings[0].range_length == 2
+    assert mappings[0].destination_range.lower == 50
+    assert mappings[0].destination_range.upper == 52
 
-    assert mappings[1].destination_range_start == 52
-    assert mappings[1].source_range_start == 50
-    assert mappings[1].range_length == 48
+    assert mappings[0].source_range.lower == 98
+    assert mappings[0].source_range.upper == 100
+
+    assert mappings[1].destination_range.lower == 52
+    assert mappings[1].destination_range.upper == 100
+    assert mappings[1].source_range.lower == 50
+    assert mappings[1].source_range.upper == 98
 
 
 def test_mapping():
-    assert 10 in Mapping(0, 0, 100)
-    assert 10 not in Mapping(11, 5, 4)
-    assert 10 in Mapping(0, 10, 1)
-    assert 10 not in Mapping(0, 9, 1)
+    mapped = Mapping(Range(0, 100), Range(0, 100)).convert_src_to_dst(Range(10, 11))
+    assert mapped[0].length == mapped[2].length == 0
+    assert mapped[1] == Range(10, 11)
 
-    assert Mapping(0, 0, 100).convert_src_to_dst(10) == 10
-    assert Mapping(0, 10, 100).convert_src_to_dst(10) == 0
-    assert Mapping(10, 0, 100).convert_src_to_dst(20) == 30
+    mapped = Mapping(Range(1000, 1050), Range(100, 150)).convert_src_to_dst(Range(50, 200))
+    assert mapped[0] == Range(50, 100)
+    assert mapped[1] == Range(1000, 1050)
+    assert mapped[2] == Range(150, 200)
 
 
 def test_map():
-    m = Map("", [Mapping(5, 10, 10), Mapping(100, 200, 1)])
-    assert m.convert_src_to_dst(-1) == -1
-    assert m.convert_src_to_dst(15) == 10
+    m = Map(
+        "",
+        [
+            Mapping(Range(1000, 1050), Range(100, 150)),
+            Mapping(Range(0, 1), Range(1001, 1002)),
+        ]
+    )
+    assert m.convert_src_to_dst(Range(101, 102)) == [Range(1001, 1002)]
+    assert m.convert_src_to_dst(Range(149, 151)) == [Range(1049, 1050), Range(150, 151)]
 
 
 def test_conversion(almanac: Almanac):
-    assert almanac.convert(79) == 82
-    assert almanac.convert(14) == 43
-    assert almanac.convert(55) == 86
-    assert almanac.convert(13) == 35
+    assert almanac.convert(Range(79, 80)) == [Range(82, 83)]
+    assert almanac.convert(Range(14, 15)) == [Range(43, 44)]
+    assert almanac.convert(Range(55, 56)) == [Range(86, 87)]
+    assert almanac.convert(Range(13, 14)) == [Range(35, 36)]
